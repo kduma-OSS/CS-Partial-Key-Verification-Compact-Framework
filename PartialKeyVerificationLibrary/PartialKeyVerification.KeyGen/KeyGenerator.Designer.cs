f7e65190e4582b3aaabcd3e6e52181be01c2c081
Krystian Duma <git@krystian.duma.sh>
2019-12-16 23:10:51 +0000

    First iteration on key generator
    git-tfs-id: [http://tfs:8080]$/Partial Key Verification Library;C35

---
+++ PartialKeyVerificationLibrary/PartialKeyVerification.KeyGen/KeyGenerator.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace PartialKeyVerification.KeyGen
+{
+    partial class KeyGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.generatedKeyTextBox = new System.Windows.Forms.TextBox();
+            this.seedTextBox = new System.Windows.Forms.TextBox();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.generatedKeyLabel = new System.Windows.Forms.Label();
+            this.seedLabel = new System.Windows.Forms.Label();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripContainer1.BottomToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.BottomToolStripPanel
+            // 
+            this.toolStripContainer1.BottomToolStripPanel.Controls.Add(this.statusStrip1);
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.generatedKeyTextBox);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.seedTextBox);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.userNameTextBox);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.generatedKeyLabel);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.seedLabel);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.userNameLabel);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(242, 95);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(242, 141);
+            this.toolStripContainer1.TabIndex = 1;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.menuStrip1);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.statusStrip1.Location = new System.Drawing.Point(0, 0);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(242, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // generatedKeyTextBox
+            // 
+            this.generatedKeyTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.generatedKeyTextBox.Location = new System.Drawing.Point(99, 63);
+            this.generatedKeyTextBox.Name = "generatedKeyTextBox";
+            this.generatedKeyTextBox.ReadOnly = true;
+            this.generatedKeyTextBox.Size = new System.Drawing.Size(131, 20);
+            this.generatedKeyTextBox.TabIndex = 0;
+            // 
+            // seedTextBox
+            // 
+            this.seedTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.seedTextBox.Location = new System.Drawing.Point(99, 37);
+            this.seedTextBox.Name = "seedTextBox";
+            this.seedTextBox.ReadOnly = true;
+            this.seedTextBox.Size = new System.Drawing.Size(131, 20);
+            this.seedTextBox.TabIndex = 0;
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.userNameTextBox.Location = new System.Drawing.Point(99, 11);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(131, 20);
+            this.userNameTextBox.TabIndex = 0;
+            this.userNameTextBox.TextChanged += new System.EventHandler(this.userNameTextBox_TextChanged);
+            // 
+            // generatedKeyLabel
+            // 
+            this.generatedKeyLabel.AutoSize = true;
+            this.generatedKeyLabel.Location = new System.Drawing.Point(12, 66);
+            this.generatedKeyLabel.Name = "generatedKeyLabel";
+            this.generatedKeyLabel.Size = new System.Drawing.Size(81, 13);
+            this.generatedKeyLabel.TabIndex = 1;
+            this.generatedKeyLabel.Text = "Generated Key:";
+            // 
+            // seedLabel
+            // 
+            this.seedLabel.AutoSize = true;
+            this.seedLabel.Location = new System.Drawing.Point(12, 40);
+            this.seedLabel.Name = "seedLabel";
+            this.seedLabel.Size = new System.Drawing.Size(35, 13);
+            this.seedLabel.TabIndex = 1;
+            this.seedLabel.Text = "Seed:";
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Location = new System.Drawing.Point(12, 14);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(63, 13);
+            this.userNameLabel.TabIndex = 1;
+            this.userNameLabel.Text = "User Name:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(242, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 141);
+            this.Controls.Add(this.toolStripContainer1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1000, 175);
+            this.MinimumSize = new System.Drawing.Size(250, 175);
+            this.Name = "Form1";
+            this.Text = "PKV Key Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStripContainer1.BottomToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.BottomToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.ContentPanel.PerformLayout();
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox seedTextBox;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.Label seedLabel;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox generatedKeyTextBox;
+        private System.Windows.Forms.Label generatedKeyLabel;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ PartialKeyVerificationLibrary/PartialKeyVerification.KeyGen/KeyGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PartialKeyVerification.Checksum;
+using PartialKeyVerification.Hash;
+
+namespace PartialKeyVerification.KeyGen
+{
+    public partial class KeyGenerator : Form
+    {
+        private readonly PartialKeyGenerator _generator;
+
+        public KeyGenerator()
+        {
+            InitializeComponent();
+            _generator = new PartialKeyGenerator(new Adler16(), new Jenkins96(), new uint[] { 1, 2, 3, 4 }) { Spacing = 6 };
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void userNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            GenerateAndSetSeedAndKey();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GenerateAndSetSeedAndKey();
+        }
+
+        private void GenerateAndSetSeedAndKey()
+        {
+            var seed = PartialKeyValidator.GetSerialNumberFromSeed(userNameTextBox.Text).ToString(CultureInfo.InvariantCulture);
+            seedTextBox.Text = seed.ToString(CultureInfo.InvariantCulture);
+            generatedKeyTextBox.Text = _generator.Generate(seed);
+        }
+    }
+}
